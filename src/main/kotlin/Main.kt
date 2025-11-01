79a2a002259af2219a1efc37e82e5ca2e724868b
Application_Belchenko <testylya333@gmail.com>
2025-11-01 11:05:49 +0300

    add base

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,47 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class triangle {
+    var x1 = 0
+    var y1 = 0;
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun getPoints(){
+        println("X1: ${x1}, Y1: ${y1}")
+    }
+}
+
+fun main() {
+
+    while (true) {
+
+        println("Choose the application (1-5)")
+        val input = readln()
+        val value = input.toIntOrNull()
+        println()
+
+        when (value) {
+            1 -> {
+
+            }
+
+            2 -> {
+
+            }
+
+            3 -> {
+
+            }
+
+            4 -> {
+
+            }
+
+            5 -> {
+
+            }
+
+            else -> {
+                println("Wrong exit")
+                break;
+            }
+        }
+        println();
+    }
 }
